9bbbbedb0663fdcbad3dc233b49b390e293c2f68
Lionel MORIN <lionel.morin@ac-dijon.fr>
2021-06-24 18:08:55 +0200

    Fix remove chosen date in rdv

--- imports/ui/routes/PollEdition/components/SelectedDatesTable.svelte
+++ imports/ui/routes/PollEdition/components/SelectedDatesTable.svelte
@@ -1,20 +1,20 @@
 <script>
-  import moment from "moment";
-  import { _ } from "svelte-i18n";
+  import moment from 'moment';
+  import { _ } from 'svelte-i18n';
 
-  import Divider from "/imports/ui/components/common/Divider.svelte";
-  import NoResults from "/imports/ui/components/common/NoResults.svelte";
+  import Divider from '/imports/ui/components/common/Divider.svelte';
+  import NoResults from '/imports/ui/components/common/NoResults.svelte';
 
   export let dates = null;
   export let meetingSlots = false;
 
   const removeDate = (date) => {
-    dates = dates.filter((d) => d.date !== date.date);
+    dates = dates.filter((d) => d !== date);
   };
 </script>
 
 <div class="box">
-  <h3 class="title is-5">{$_("pages.new_poll_2.selected_dates")}</h3>
+  <h3 class="title is-5">{$_('pages.new_poll_2.selected_dates')}</h3>
   <Divider />
   {#if dates.length}
     <table class="table is-striped is-fullwidth">
@@ -23,19 +23,16 @@
           <tr>
             <th>
               {#if meetingSlots}
-                {moment(date.start).format($_("components.Time.dateFormat"))} /
-                {moment(date.start).format("HH:mm")}
+                {moment(date.start).format($_('components.Time.dateFormat'))} /
+                {moment(date.start).format('HH:mm')}
                 -
-                {moment(date.end).format("HH:mm")}
+                {moment(date.end).format('HH:mm')}
               {:else}
-                {moment(date.date).format($_("components.Time.dateFormat"))}
+                {moment(date.date).format($_('components.Time.dateFormat'))}
               {/if}
             </th>
             <td>
-              <button
-                on:click={() => removeDate(date)}
-                class="button is-rounded is-primary is-small"
-              >
+              <button on:click={() => removeDate(date)} class="button is-rounded is-primary is-small">
                 <span class="icon is-small">
                   <i class="fas fa-trash" />
                 </span>
@@ -46,7 +43,7 @@
       </tbody>
     </table>
   {:else}
-    <NoResults title={$_("pages.new_poll_2.no_selected_dates")} />
+    <NoResults title={$_('pages.new_poll_2.no_selected_dates')} />
   {/if}
 </div>
 
