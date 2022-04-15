f2225b9cd6268840a99bd027aa4282f90242ce86
Bruno Boiget <bruno.boiget@ac-dijon.fr>
2022-04-15 11:10:08 +0200

    feat(maintenance): lock application if laboite is in maintenance mode

---
+++ imports/ui/routes/Maintenance.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { _ } from 'svelte-i18n';
+
+  export let textMaintenance = '';
+</script>
+
+<section class="hero is-medium is-grey is-bold is-centered">
+  <div class="hero-body">
+    <div class="container has-text-centered">
+      <div class="title is-4">{$_('pages.maintenance.title')}</div>
+      <div class="title is-5">{$_(textMaintenance)}</div>
+    </div>
+  </div>
+</section>
+
+<style>
+  .title {
+    text-align: center;
+  }
+</style>
