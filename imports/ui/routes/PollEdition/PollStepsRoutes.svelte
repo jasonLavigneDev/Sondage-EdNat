288d026cd39548bb27ec08357721351090365fda
Bruno Boiget <bruno.boiget@ac-dijon.fr>
2022-08-30 17:16:18 +0200

    feat(autologin): allow to log in and proceed on poll creation

--- imports/ui/routes/PollEdition/PollStepsRoutes.svelte
+++ imports/ui/routes/PollEdition/PollStepsRoutes.svelte
@@ -1,6 +1,7 @@
 <script>
   import { Route } from 'tinro';
 
+  import { ROUTES } from '/imports/utils/enums';
   import InfoStep from './InfoStep.svelte';
   import DateStep from './DateStep.svelte';
   import TimeStep from './TimeStep.svelte';
@@ -8,33 +9,40 @@
   import { newPollStore } from '/imports/utils/functions/stores';
   import { POLLS_TYPES } from '/imports/utils/enums';
   import CalendarStep from './CalendarStep.svelte';
+  import { currentUser, loggingIn, accountsConfigured } from '/imports/utils/functions/stores';
 
   export let redirect;
   export let meta;
+
+  $: if (!$currentUser && !$loggingIn && $accountsConfigured === true) {
+    Meteor.loginWithKeycloak();
+  }
 </script>
 
-<Route path="/1">
-  <InfoStep {meta} />
-</Route>
+{#if $currentUser}
+  <Route path="/1">
+    <InfoStep {meta} />
+  </Route>
 
-<Route path="/2">
-  {#if $newPollStore.type === POLLS_TYPES.POLL}
-    <DateStep {meta} />
-  {:else}
-    <CalendarStep {meta} />
-  {/if}
-</Route>
+  <Route path="/2">
+    {#if $newPollStore.type === POLLS_TYPES.POLL}
+      <DateStep {meta} />
+    {:else}
+      <CalendarStep {meta} />
+    {/if}
+  </Route>
 
-<Route path="/3">
-  {#if $newPollStore.type === POLLS_TYPES.POLL}
-    <TimeStep {meta} />
-  {:else}
+  <Route path="/3">
+    {#if $newPollStore.type === POLLS_TYPES.POLL}
+      <TimeStep {meta} />
+    {:else}
+      <ValidationStep {meta} />
+    {/if}
+  </Route>
+
+  <Route path="/4">
     <ValidationStep {meta} />
-  {/if}
-</Route>
+  </Route>
 
-<Route path="/4">
-  <ValidationStep {meta} />
-</Route>
-
-<Route path="*" {redirect} />
+  <Route path="*" {redirect} />
+{/if}
